3af6b6dfb0238b6747397b14f63e77d4ae8408f4
gabrielrial <gabrielrialpicco@gmail.com>
2025-07-04 17:55:32 +0200

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,28 +1,75 @@
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
-Fixed::Fixed() {
-    _fixed_point = 0;
-    std::cout << "Default constructor called" << std::endl;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_fixed_point = 0;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
+Fixed::Fixed(const int n)
+{
+	std::cout << "Int constructor called" << std::endl;
+	if (n > (INT_MAX / (1 << _factorial_bit)))
+	{
+		std::cerr << "Warning: integer too large, value reset to 0" << std::endl;
+		_fixed_point = 0;
+		return;
+	}
+	_fixed_point = n << _factorial_bit;
 }
 
-Fixed& Fixed::operator=(const Fixed& value) {
-    std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &value)
-        _fixed_point = value.getRawBits();
-    return *this;
+Fixed::Fixed(const float f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixed_point = roundf(f * (1 << _factorial_bit));
 }
 
-int Fixed::getRawBits() const {
-    std::cout << "getRawBits member function called" << std::endl;
-    return _fixed_point;
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_fixed_point = other.getRawBits();
 }
 
-void Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called" << std::endl;
-    _fixed_point = raw;
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& value)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &value)
+		_fixed_point = value.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _fixed_point;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_fixed_point = raw;
+}
+
+int Fixed::toInt(void) const
+{
+	return _fixed_point >> _factorial_bit;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float)_fixed_point / (1 << _factorial_bit);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
+{
+	os << fixed.toFloat();
+	return os;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,18 +1,30 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
-	public:
-		Fixed();
-		~Fixed();
+public:
+	Fixed();
+	Fixed(const int);
+	Fixed(const float);
+	Fixed(const Fixed &);
+	~Fixed();
 
-		int getRawBits( void ) const;
-		void setRawBits(int const raw);
+	Fixed &operator=(const Fixed &);
 
-	private:
-		int _fixed_point;
-		static const int _factorial_bit = 8;
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
+
+	friend std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+private:
+	int _fixed_point;
+	static const int _factorial_bit = 8;
 };
 
 #endif
